856d457a8c2287a50e8c47c9c305d2193f861d71
Applepi7 <dhwjddnjs2000@naver.com>
2017-11-26 22:24:15 +0900

    Complete player movement

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+    private float sensitivity = 2.0f;
+    private float smoothing = 2.0f;
+
+    [SerializeField]
+    private GameObject player;
+
+	void Update () {
+        Vector2 mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDirection = Vector2.Scale(mouseDirection, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDirection.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDirection.y, 1f / smoothing);
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update ()
+    {
+        float hAxis = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vAxis = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(hAxis, 0f, vAxis);
+	}
+}
